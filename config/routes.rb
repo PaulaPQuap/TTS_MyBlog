32d96259d5c225409b76c5bb7cf0697ce278b771
Ben Davis <bendavis78@gmail.com>
2016-04-21 10:43:39 -0500

    updated with more security

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+
   resources :comments
   resources :blog_posts
 
-  get '/contact-us' => 'welcome#contact_us'
+  get 'contact-us' => 'welcome#contact_us'
+  get 'your_posts' => 'blog_posts#your_posts'
+  get 'user_posts/:id' => 'blog_posts#user_posts'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
